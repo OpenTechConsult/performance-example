d11fb1f0036095ff9ad187be553928f3a08a9f30
OpenTech-Consult <opentech.consult@gmail.com>
2022-11-03 11:27:39 +0000

    clustering in action

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cluster = require('cluster')
 
 const app = express()
 const port = 3000
@@ -10,11 +11,18 @@ function delay(duration) {
     }
 }
 
-app.get('/', (req, res) => res.send('Performance example!'))
+app.get('/', (req, res) => res.send(`Performance example: ${process.pid}`))
 
 app.get('/timer', (req, res) => {
     // delay the response
     delay(9000)
-    res.send('ding ding ding')
+    res.send(`ding ding ding ${process.pid}`)
 })
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+if (cluster.isMaster) {
+    console.log('Master has been started')
+    cluster.fork()
+    cluster.fork()
+} else {
+    console.log('Worker process started')
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+}
