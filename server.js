a25a01d307a4f754552b9162482d86ea8836eb84
OpenTech-Consult <opentech.consult@gmail.com>
2022-11-03 16:07:55 +0000

    remove unecessary node built-in cluster code in order to let pm2 handle that for us

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express')
-const cluster = require('cluster')
-const os = require('os')
 
 const app = express()
 const port = 3000
@@ -15,17 +13,10 @@ function delay(duration) {
 app.get('/', (req, res) => res.send(`Performance example: ${process.pid}`))
 
 app.get('/timer', (req, res) => {
-    // delay the response
-    delay(9000)
-    res.send(`ding ding ding ${process.pid}`)
+    delay(4000)
+    res.send(`Beep beep beep${process.pid}`)
 })
-if (cluster.isMaster) {
-    console.log('Master has been started')
-    const NUM_WORKERS = os.cpus().length
-    for (let i = 0; i < NUM_WORKERS; i++) {
-        cluster.fork()
-    }
-} else {
-    console.log('Worker process started')
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-}
+
+console.log('Running server.js...')
+console.log('Worker process started')
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
